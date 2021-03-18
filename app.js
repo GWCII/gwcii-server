416427dc97df80ee8fbc73310c5a916aec634ac2
hilmanauz <hilmanauz@gmail.com>
2021-03-19 00:26:44 +0700

    add rooms in createRoom

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const cors = require('cors');
 
 const port = process.env.PORT || 3000
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Helloword');
 });
 
+let rooms = [];
+
 //* Listen
 io.on('connection', (socket) => {
   
-  socket.on('test', (data) => {
-    io.emit('test', data)
-    console.log('user added');
+  socket.on('login', (data) => {
+    io.emit('getRooms', rooms)
+  })
+
+  socket.on('createRoom', (data) => {
+    let room  = {
+      name: data.name,
+      users: [],
+      admin: data.admin
+    }
+    rooms.push(room);
+    io.emit('updatedRoom', rooms);
   })
 
 });
