c8e3d31d1b547f1a802d692b44f5d8942ee89345
caturprasatya <caturprasatya1224@gmail.com>
2021-03-19 14:15:48 +0700

    add cors to deploy

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express()
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000
+const cors = require(cors)
 
+app.use(cors())
 
 const soal = require('./soal.json')
 let rooms = []
