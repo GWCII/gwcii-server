7cb52295083c3f6619c9614a7dc7b69ca8eb1bc8
caturprasatya <caturprasatya1224@gmail.com>
2021-03-19 14:20:20 +0700

    add cors again hndle error

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000
-const cors = require(cors)
+const cors = require('cors')
 
 app.use(cors())
 
